fbd13dbdddc96b6b5a7bcd76934c8c21752dfaeb
healban616 <susomoon0616@gmail.com>
2025-05-30 09:33:39 +0900

    Update game.py
    상위 클래스 아래 하위 클래스 추가

--- game.py
+++ game.py
@@ -6,16 +6,23 @@ class RSP:
         self._score = 0
         self._round = 5
         
+class rsp_rule(RSP):
+    def __init__(self):
+        super().__init__()
+        self._rule = {
+            '가위': '보',
+            '바위': '가위',
+            '보': '바위'
+        }
+    
     def play(self):
         print('가위바위보 ai를 이겨라🤖!!')
         print('※ 게임 중 "#"을 누르면 종류합니다')
 
-        
         for i in range(self._round):
             print('Round {}'.format(i+1))
             choice = input('가위,바위,보 중 하나를 쓰시오 :').strip()
-            rounds = random.sample(self.rsp,k = 1)
-            
+            rounds = random.choice(list(self.rsp))
 
             print('ai의 선택 : {}'.format(rounds))
 
@@ -23,15 +30,7 @@ class RSP:
                 print('가위바위보 게임을 종류합니다')
                 break
 
-            if choice == '가위' and rounds == '보':
-                print('이기셨습니다!!😀')
-                self._score += 1
-
-            elif choice == '바위' and rounds == '가위':
-                print('이기셨습니다!!😀')
-                self._score += 1
-
-            elif choice == '보' and rounds == '바위':
+            if choice == self._rule[rounds]:
                 print('이기셨습니다!!😀')
                 self._score += 1
 
@@ -40,8 +39,7 @@ class RSP:
 
             else:
                 print('허헉 지셨네요 다음엔 이겨보아요! 😅')
-        print('게임종류!!!')
-        print('이긴 Round {} / {}'.format(self._score, self._round))              
-
+        print('게임종료!!!')
+        print('이긴 Round {} / {}'.format(self._score, self._round)) 
 
         
